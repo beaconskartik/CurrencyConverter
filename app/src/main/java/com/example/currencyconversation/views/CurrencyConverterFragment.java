93d717c2eb556bb0d16b51db8473582c16393f3b
Kartik Sachan <you@example.com>
2019-08-28 01:41:48 +0530

    simiplying xml file

--- app/src/main/java/com/example/currencyconversation/views/CurrencyConverterFragment.java
+++ app/src/main/java/com/example/currencyconversation/views/CurrencyConverterFragment.java
@@ -17,53 +17,29 @@ import com.example.currencyconversation.R;
 import com.example.currencyconversation.viewModels.VmCurrencyConverter;
 import com.example.currencyconversation.viewModels.VmLocator;
 
-/**
- * A fragment representing a list of Items.
- * <p/>
- * Activities containing this fragment MUST implement the {@link OnListFragmentInteractionListener}
- * interface.
- */
 public class CurrencyConverterFragment extends Fragment {
 
-    // TODO: Customize parameter argument names
-    private static final String ARG_COLUMN_COUNT = "column-count";
-    // TODO: Customize parameters
-    private int mColumnCount = 1;
     private OnListFragmentInteractionListener mListener;
     private RecyclerView recyclerView;
     private CurrencyConverterAdapter adapter;
     private VmCurrencyConverter currencyConverter;
 
-    /**
-     * Mandatory empty constructor for the fragment manager to instantiate the
-     * fragment (e.g. upon screen orientation changes).
-     */
     public CurrencyConverterFragment() {
     }
 
-    // TODO: Customize parameter initialization
-    @SuppressWarnings("unused")
     public static CurrencyConverterFragment newInstance() {
         CurrencyConverterFragment fragment = new CurrencyConverterFragment();
-        Bundle args = new Bundle();
-        args.putInt(ARG_COLUMN_COUNT, 1);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        if (getArguments() != null) {
-            mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
-        }
-
         currencyConverter = VmLocator.getInstance().getVmCurrencyConverter();
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_item_list, container, false);
 
@@ -71,11 +47,7 @@ public class CurrencyConverterFragment extends Fragment {
         if (view instanceof RecyclerView) {
             Context context = view.getContext();
             recyclerView = (RecyclerView) view;
-            if (mColumnCount <= 1) {
-                recyclerView.setLayoutManager(new LinearLayoutManager(context));
-            } else {
-                recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
-            }
+            recyclerView.setLayoutManager(new LinearLayoutManager(context));
             adapter = new CurrencyConverterAdapter(mListener);
             recyclerView.setAdapter(adapter);
         }
@@ -118,18 +90,7 @@ public class CurrencyConverterFragment extends Fragment {
         return adapter;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnListFragmentInteractionListener {
-        // TODO: Update argument type and name
         void onListFragmentInteraction(Currency item, int pos);
     }
 }
